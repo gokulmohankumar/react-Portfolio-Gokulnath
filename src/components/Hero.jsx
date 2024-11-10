7accea79b31ab8e06902d34743719158b75e47a4
Gokulnath <137893923+gokulmohankumar@users.noreply.github.com>
2024-11-10 23:14:35 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
               variants={container(1)}
               initial='hidden'
               animate='visible'
-              className='my-2 max-w-xl py-6 font-light ml-[40px]  text-white text-lg tracking-normal'>{HERO_CONTENT}</motion.p>
+              className='my-2 max-w-xl py-6 font-light ml-[40px]  text-white text-lg tracking-normal text-justify'>{HERO_CONTENT}</motion.p>
              </div>
           </div>
           <div className='w-full lg:w-1/2 p-8 '>
