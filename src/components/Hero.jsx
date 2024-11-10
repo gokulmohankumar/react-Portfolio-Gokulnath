2cca5c02b5c7fbdc5244c0ffeb391ab2b6d926d0
Gokulnath <137893923+gokulmohankumar@users.noreply.github.com>
2024-11-10 23:23:00 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
                 initial={{x:100,opacity:0}}
                 animate={{x:0,opacity:1}}
                 transition={{duration:1, delay:1.2}}
-                 className='rounded-md'
+                className='rounded-md'
                 src={profilePic} alt="profile picture" />
                
               </div>
